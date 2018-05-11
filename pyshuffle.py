a863925e5fcdae7c529e1c2ff837a0db40fd6d40
FreddeG <fredrik.ano@gmail.com>
2018-05-11 13:32:16 +0200

    test

--- pyshuffle.py
+++ pyshuffle.py
@@ -4,12 +4,17 @@ import os
 
 
 
-data = np.genfromtxt('./mid_values.txt', delimiter=',')
+data = np.genfromtxt('/home/fredde/git-repos/stairNet/datasets/04-05-downst1.txt', delimiter=',')
 print("Non-Shuffled data:")
 print(data)
 
-
+data = data[~np.isinf(data).any(axis=1)]
 np.random.shuffle(data)
 print("Shuffled data:\n")
 print(data)
-np.savetxt("testshuffle.txt", data, delimiter=',', fmt='%.3f')
+
+#np.savetxt("testshuffle.txt", data, delimiter=',', fmt='%.3f')
+
+np.savetxt("/home/fredde/git-repos/stairNet/testshuffle.txt", data, delimiter=',', fmt='%.3f')
+
+#test
